40cad50fb912067b7069a121b78dd1997f51aa2d
Rafoevistep <rafovirabyanevistep@gmail.com>
2022-08-31 17:50:32 +0400

    admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +16,29 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::truncate();
+        $users = [
+            [
+                'last_name' => 'Admin',
+                'first_name' => 'Admin First',
+                'email' => 'admin@gmail.com',
+                'password' => '12345678',
+                'phone' => '77777777',
+                'is_admin' => '1',
+            ],
+        ];
+
+        foreach($users as $user)
+        {
+            User::create([
+                'last_name' => $user['last_name'],
+                'first_name' => $user['first_name'],
+                'email' => $user['email'],
+                'password' => Hash::make($user['password']),
+                'phone' => $user['phone'],
+                'is_admin' => $user['is_admin'],
+            ]);
+        }
     }
 }
